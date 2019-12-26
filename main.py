fdde68c812eeb0493dcfe1b4db4da3acac2c1238
Akshays <49100604+akshays-repo@users.noreply.github.com>
2019-12-26 23:58:48 +0530

    code optimized

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas
 import json
-import datetime
 import sendmail
+from datetime import datetime
 
 #convert excel data to json then to Dict 
 excel_data = pandas.read_excel('dob.xlsx')
@@ -9,13 +9,12 @@ json_data = excel_data.to_json(orient='records')
 data = json.loads(json_data)
 
 def checkbirtday(data,choice):
-    for person in data:
-        # Assign user info to variables 
+    for person in data: 
         try:
-            date_of_birth = person['DOB']
-            day = date_of_birth[0:2]
-            month = date_of_birth[3:5]
-            year = date_of_birth[6:10]
+            dateofbirth_srt = str(person['DOB'])
+            dateofbirth_object=datetime.strptime(dateofbirth_srt,'%d:%m:%y')
+            day = dateofbirth_object.day
+            month = dateofbirth_object.month
             email_id =person['EMAIL']
             name = person['NAME']
         except TypeError:
@@ -23,31 +22,30 @@ def checkbirtday(data,choice):
         if choice == '1':
             if person['DOB'] != None:
                 print('*' * 20)
-                print("Name :" ,name)
-                print("Email :",email_id)
-                print('Date of Birth :',date_of_birth)
+                print(f'Name :{name}')
+                print(f'Email :{email_id}')
+                print(f'Date of Birth :{dateofbirth_object}')
             else:
                 pass
-        elif choice == '2' and date_of_birth != None:
+        elif choice == '2' and dateofbirth_srt != None:
             #get current day and month
-            d =datetime.datetime.today()
+            d =datetime.today()
             currentmonth = d.month
             currentday = d.day
             
             #check today is bday or not 
             if str(day) == str(currentday) and str(month) == str(currentmonth):
-                print("Today is ",name.title()+' birtday.')
-                print("Sending a Wish to ",name +"." )
+                print(f'Today is {name.title()} birtday.')
+                print(f'Sending a Wish to {name}.' )
                 sendmail.send_mail(name,email_id)  #send mail 
             else:
-                print("Today is not ",name +' birtday.')
+                pass
         else:
             pass
 
 
 #Begin 
-
-print('\n\nWelcome to BIRTHDAY_EMAIL BOT')
+print('\n\nWelcome to PYTHON BIRTH BOT')
 print(' ****************************')
 choice = '1'
 while choice != '3':
